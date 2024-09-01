9eebe3badc3eeb7013747e6ffff2b8cbaf88d081
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2024-09-01 00:21:21 -0700

    add elemants for the center

--- masara/src/components/about/center.js
+++ masara/src/components/about/center.js
@@ -1,11 +1,23 @@
-
+import "../about/about.css"
+import { Container, Row, Col, Card } from 'react-bootstrap';
+ 
 
 
 const Center = ()=>{
     return (
-        <div>
-            <h1>its okay</h1>
-        </div>
+    <><Container className="mt-4 custom-shadow">
+            <Row className='top  align-items-center'>
+                <Col md={4} className="text-center text-white fw-bold custom-text"><p>Our center</p></Col>
+            </Row>
+        </Container>
+         <Container className="mt-3">
+                <Card>
+                    <Card.Body >
+                        <p className="text-center fw-bold fs-5">At MASRA, we believe in the power of personalized learning and the importance of
+                            fostering a love of discovery. We're a community of passionate educators and supportive families dedicated to empowering children to thrive in a nurturing environment. </p>
+                    </Card.Body>
+                </Card>
+            </Container></> 
     )
 }
 
