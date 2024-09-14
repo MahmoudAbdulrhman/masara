a8d97bd5a612172c65121f228895801cb9fef273
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2024-09-13 23:19:58 -0700

    update

--- masara/src/components/mainPage/hero.js
+++ masara/src/components/mainPage/hero.js
@@ -1,22 +1,3 @@
-// import React from "react";
-// import { Card } from "react-bootstrap";
-import "../../CustomStyle.css"; // Import your custom CSS
-
-// function Hero() {
-//   return (
-//     <div className="hero-body">
-//       <Card className="hero-section">
-//         <Card.Body className="d-flex flex-column align-items-center">
-//           <p className="display-4 white-heading fontSize">MASRA</p>
-//           <p className="white-heading">HOMESCHOOLING</p>
-//         </Card.Body>
-//       </Card>
-//     </div>
-//   );
-// }
-
-// export default Hero;
-
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../../CustomStyle.css"; // Import your custom CSS
